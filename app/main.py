58f5fd2a211631ab733a8cd1dccb168ef8b03b54
vavarvariuk <vasya.varvariuk.27@gmail.com>
2024-10-24 20:07:47 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -41,9 +41,36 @@ class Battleship:
             self.matrix[x][y] = "*"
             for i in self.ship:
                 a, b = i
-                if (self.matrix[a[0]][a[1]] == "*"
-                        and self.matrix[b[0]][b[1]] == "*"):
+                print(b[1] - a[1])
+                if a[0] == b[0] and b[1] - a[1] == 1:
+                    return "Hit"
+                if self.matrix[a[0]][a[1]] == "*" and self.matrix[b[0]][b[1]] == "*":
                     return "Sunk!"
             return "Hit!"
         elif self.matrix[x][y] != u"\u25A1":
             return "Miss!"
+
+# battle_ship = Battleship(
+#         ships=[
+#             ((2, 0), (2, 3)),
+#             ((4, 5), (4, 6)),
+#             ((3, 8), (3, 9)),
+#             ((6, 0), (8, 0)),
+#             ((6, 4), (6, 6)),
+#             ((6, 8), (6, 9)),
+#             ((9, 9), (9, 9)),
+#             ((9, 5), (9, 5)),
+#             ((9, 3), (9, 3)),
+#             ((9, 7), (9, 7)),
+#         ]
+#     )
+#
+#
+# print(battle_ship.fire((0, 4))) #== "Miss!"
+# print(battle_ship.fire((1, 7))) # == "Miss!"
+# print(battle_ship.fire((2, 0))) # == "Hit!"
+# print(battle_ship.fire((2, 1))) #== "Hit!"
+# print(battle_ship.fire((2, 2))) #== "Hit!"
+# print(battle_ship.fire((2, 3)))  #== "Sunk!"
+# print(battle_ship.fire((4, 3)))  #== "Miss!"
+# print(battle_ship.fire((4, 5))) #== "Hit!"
